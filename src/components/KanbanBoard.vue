a5406cf60c4bb5fb6cc0e918de9442b422e2d2e0
DenSonmez <113019821+DenSonmez@users.noreply.github.com>
2024-05-06 20:34:54 +0200

    function for editing and deleting tasks

--- src/components/KanbanBoard.vue
+++ src/components/KanbanBoard.vue
@@ -1,15 +1,49 @@
 <template>
   <v-container class="cus-background">
     <!-- gridkomponent -->
-    <v-col cols="12">
+    <v-col cols="12"> <!-- 12 kolonner i gridet -->
       <h1 class="text-center">Kanban Board</h1>
     </v-col>
-    
-    <v-row>
-      <v-col v-for="(column, index) in arrColumns" :key="index" cols="4">
-        
-        <!-- Funktionen til tilføje task -->
-         <div v-if="column.showInputField" class="add-button-container text-center">
+
+    <v-row class="mt-5"> <!-- for skabe lidt aftand -->
+      <v-col v-for="(column, index) in arrColumns" :key="index" cols="3"> <!-- opretter en kolonne til hver kanban-kolonne -->
+        <v-card class="p-2" :color="column.color">
+          <h2 class="text-center">{{ column.title }}</h2>
+
+          <!-- træk og slip komponent -->
+          <draggable class="list-group kanban-column" :list="column.tasks" group="tasks" itemKey="uniqueId" v-slot="{ element, index}">
+            <template>
+              <v-card >
+                <v-card-text>
+                  <template v-if="!element.editingTitle">
+                    <h4 @click="editTask('title', index, column)">{{ element.title }}</h4>
+                    <v-icon class="icon" @click="editTask('title', index, column)">mdi-pencil</v-icon>
+                  </template>
+                  <template v-else>
+                    
+                    <v-text-field v-model="element.title" @blur="saveTask('title', index, column)"></v-text-field>
+                  </template>
+                </v-card-text>
+
+                <v-card-text>
+                  <template v-if="!element.editingDescription">
+                    <p @click="editTask('description', index, column)">{{ element.description }}</p>
+                    <v-icon class="icon" @click="editTask('description', index, column)">mdi-pencil</v-icon>
+                  </template>
+                  <template v-else>
+                    <v-text-field v-model="element.description" @blur="saveTask('description', index, column)"></v-text-field>
+                  </template>
+                </v-card-text>
+                
+                <v-btn @click="removeTask(column.tasks, element)" small class="float-right">
+                  <v-icon size="25">mdi-delete</v-icon>
+                  </v-btn>
+              </v-card>
+            </template>
+          </draggable>
+          
+          <!-- Funktionen til tilføje task -->
+          <div v-if="column.showInputField" class="add-button-container text-center">
           <v-text-field v-model="column.newTask.title" required placeholder="Title"></v-text-field>
           <v-text-field v-model="column.newTask.description" placeholder="Description"></v-text-field>
           <v-btn @click="addTask(column)" color="#381010" :disabled="!column.newTask.title.trim()">
@@ -18,12 +52,14 @@
           </div>
           <div v-else class="text-center">
             <v-btn @click="column.showInputField = true" color="#381010">
-            <v-icon class="ml-1">mdi-plus</v-icon>
+            <v-icon class="ml-2">mdi-plus</v-icon>  
             <span style="text-transform: none;">
             {{ column.tasks.length > 0 ? 'Add another Card' : 'Add a Card' }} <!-- hvis der er opgaver i kolonnen, så skriv "Add another Card" ellers "Add a Card" -->
             </span>
             </v-btn>
           </div>
+          
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -43,10 +79,7 @@ export default {
       arrColumns: [
         { title: "Backlog", color: "warning",
         tasks: [], newTask: { title: "", description: "" }},
-
-        { title: "Nice Yo Have", color: "green",
-        tasks: [], newTask: { title: "", description: "" }},
-
+        
         { title: "To Do", color: "secondary", 
         tasks: [], newTask: { title: "", description: "" }},
 
@@ -82,11 +115,11 @@ export default {
         tasks.splice(index, 1);
       }
     },
-    editTaks(field, index, column) {
-      column.tasks[index][editing$] = true;
+    editTask(field, index, column) {
+      column.tasks[index][`editing${field.charAt(0).toUpperCase() + field.slice(1)}`] = true;
     },
     saveTask(field, index, column) {
-      column.tasks[index][editing$] = false;
+      column.tasks[index][`editing${field.charAt(0).toUpperCase() + field.slice(1)}`] = false;
     }
   }
 
