8d992fb3048fc57e8597dffa82586f30e58fef3f
DenSonmez <113019821+DenSonmez@users.noreply.github.com>
2024-05-06 15:47:22 +0200

    styling background and data to create columns and tasks

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,14 @@
   <v-app>
     <v-main>
       <KanbanBoard />
+      <Navbar />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
 import KanbanBoard from './components/KanbanBoard.vue';
-
+import './styles.css'; 
 
 
 </script>
---
+++ src/components/KanbanBoard.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container class="cus-background">
+    <!-- gridkomponent -->
+    <v-col cols="12">
+      <h1 class="text-center">Kanban Board</h1>
+    </v-col>
+
+  </v-container>
+</template>
+
+
+
+<script>
+
+export default {
+  name: "kanban-board",
+
+  components: {
+
+  },
+ // data til at oprette kolonner og opgaver
+  data() {
+    return {
+      arrColumns: [
+        { title: "Backlog", color: "warning",
+        tasks: [], newTask: { title: "", description: "" }},
+
+        { title: "Nice Yo Have", color: "green",
+        tasks: [], newTask: { title: "", description: "" }},
+
+        { title: "To Do", color: "secondary", 
+        tasks: [], newTask: { title: "", description: "" }},
+
+        { title: "In Progress", color: "success", 
+        tasks: [], newTask: { title: "", description: "" }},
+
+        { title: "Done", color: "primary", 
+        tasks: [], newTask: { title: "", description: "" }}
+      ]
+    };
+  },
+
+
+  }
+
+
+</script>
